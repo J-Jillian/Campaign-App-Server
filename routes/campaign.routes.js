50b9acd154e0498484df9f301d2568714107fdc3
j. jillian <judith.mediosdigitales@gmail.com>
2022-06-22 16:31:01 +0200

    CRUD DONE

--- routes/campaign.routes.js
+++ routes/campaign.routes.js
@@ -17,4 +17,75 @@ router.get('/:campaignId', async(req, res, next)=>{
 
 //Create a campaign
 
-routr.post ('')
+router.post ('/', async (req, res, next)=> {
+const {CampaignName, place, image, description, foundsFor, creator } = req.body
+  try {
+    const campaign = await Campaign.create({
+        CampaignName : CampaignName.trim(),
+        place : place.trim(),
+        description: description,
+        image: image,
+        foundsFor : foundsFor,
+        creator: creator
+ })
+    
+  } catch (error) {
+    
+  }  
+})
+
+// Update a campaign 
+
+ router.put ('/:campaignId', async (req, res, next) => {
+    const { campaignId } = req.params
+    const {CampaignName, place, image, description, foundsFor, creator, totalAmount } = req.body
+    
+    const newData = {}
+
+    if(CampaignName !==''){
+        newData.CampaignName.trim()
+    }
+
+    if(place !==''){
+        newData.place = place.trim()
+    }
+
+    if(image !==''){
+        newData.image = image
+    }
+
+    if(description !==''){
+        newData.description = description.trim()
+    }
+
+    if(foundsFor !==''){
+        newData.foundsFor = foundsFor.trim()
+    }
+
+    if(creator !==''){
+        newData.creator = creator.trim()
+    }
+
+    if(totalAmount !=='0'){
+        newData.totalAmount = parseFloat(totalAmount)
+    }
+
+    try {
+     const campaign = await Campaign.findByIdAndUpdate(campaignId, newData)  
+    res.status(200).json({message: 'Campaign is updated', id: campaign.id })
+    } catch (error) {
+        res.status(500).json(error)  
+    }
+ })
+
+ //Detele a campaign
+ router.delete('/:campaignId', async (req, res, next) => {
+    const {campaignId} = req.params
+
+await Campaign.findByIdAndDelete(campaignId)
+res.status(200).json({message: 'Campaign is updated'})
+
+
+ })
+
+ module.exports = router
