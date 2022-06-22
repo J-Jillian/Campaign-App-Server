52e1b9fe84fdf1750b0e0a79710c5d5e8c9ac360
Hanin Mez <haninjamalmzher@gmail.com>
2022-06-22 15:48:42 +0200

    do models

---
+++ models/Campaign.model.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const campaignSchema = new Schema(
+  {
+    CampaignName: {
+      type: String,
+      unique: true,
+    },
+    place: String,
+    case: [{
+      type: String,
+    }],
+    totalAmount: Number,
+    creator: [{type: Schema.Types.ObjectId, ref: 'User'}],
+    
+    donations:[{ 
+      Amount: Number,
+      Author: [{type: Schema.Types.ObjectId, ref: 'User'}],
+    }]   
+},
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const Campaign = model("Campaign", campaignSchema);
+
+module.exports = Campaign;
--- models/User.model.js
+++ models/User.model.js
@@ -3,17 +3,29 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    username: {
-      type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
-    },
+    username: String,
     password: String,
-  },
-  {
+  
+    campaignDonated: [{
+      Campaign: [{type: Schema.Types.ObjectId, ref: 'Campaign'}],
+      type: String,
+    }],
+    createdCampaign: [{
+      type: String,
+      unique: true,
+      total : Number,
+    }],
+    FavCampaigns: [{ 
+      Campaign: [{type: Schema.Types.ObjectId, ref: 'Campaign'}],
+      type: String,
+    }],
+ },
     // this second object adds extra properties: `createdAt` and `updatedAt`
-    timestamps: true,
-  }
-);
+   {
+     timestamps: true,
+   }
+  
+)
 
 const User = model("User", userSchema);
 
